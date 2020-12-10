8a10df5e5b062bc3c4fdce4a028bd739ba4a6500
Dmitriy <70809857+Ruiner639@users.noreply.github.com>
2020-12-10 19:19:46 +0300

    added description

--- Program 2+/Program.cs
+++ Program 2+/Program.cs
@@ -7,25 +7,25 @@ namespace Program_2_
         static void Main(string[] args)
         {
            
-            var hour = DateTime.Now.Hour;
-            string hour_string = "eror";
-            var day = DateTime.Now;
-            var monday = "07.12.2020";
-            var day_m = DateTime.Parse(monday);
-            var today = 10;
-            int i = 1;
-            while (today != i)
+            var hour = DateTime.Now.Hour;//I entered hours to determine the time of day
+            string hour_string = "eror";//To display one of the lines
+            var day = DateTime.Now;//date at the moment
+            var monday = "07.12.2020";//day from which the week is counted by day
+            var day_m = DateTime.Parse(monday);//convert string to date value
+            int today;//entered a value to which the number of the day of the week will be assigned
+            int i = 1;//the days of the week started from one
+            while (today != i)//cycle defining the number of the day of the week
             {
-              if (day.DayOfWeek == day_m.DayOfWeek)
+              if (day.DayOfWeek == day_m.DayOfWeek)//I don’t know how to bring the time into a string, so I decided to go from the opposite and use the selection method to calculate the day
                 {
                     today = i;
                 }
                 else
                 {
-                    day_m = day_m.AddDays(1);
+                    day_m = day_m.AddDays(1);//I am adding 1 day so that the loop finds the correct today's date number
                     i++;
                 }
-            } if((hour >= 4) && (hour <= 11))
+            } if((hour >= 4) && (hour <= 11))//simple condition for finding the time of day
             {
                 hour_string = "Good morning, ";
             }
@@ -37,7 +37,7 @@ namespace Program_2_
             {
                 hour_string = "Good evening, ";
             }
-            if ((today - 5) <= 0)
+            if ((today - 5) <= 0)//determining whether the day is a work day or a day off
             {
                 Console.WriteLine(hour_string + "it is a lovely " + day.DayOfWeek + " today. Weekends is coming in " + (6 - today) + " days");
             }
